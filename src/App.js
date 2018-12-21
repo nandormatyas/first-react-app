89fb27c1a4a13ec531366f98b1addbaf4dc73a8e
nandor matyas <nandor.matyas@gmail.com>
2018-12-21 12:30:57 +0100

    Feat(react): modifying root state with functions as props

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 } 
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    console.log(ninjas);
+    this.setState({
+      ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first react app!</h1>
         <p>Welcome!</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
