379f28b43442b82bd844024e1a8da186ac27bb69
nandor matyas <nandor.matyas@gmail.com>
2018-12-21 11:54:04 +0100

    Feat(react): props, map, if

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = { 
+    ninjas: [ 
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 } 
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>My first react app!</h1>
+        <p>Welcome!</p>
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
